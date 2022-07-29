48972e97ac481091cb107a0aa91d27fe60e0d844
NipunGoel1 <66476112+NipunGoel1@users.noreply.github.com>
2022-07-29 17:04:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlL1v0LFhRyzQ--icewrGUWSzHxXT69wc",
+  authDomain: "storytelling-679f0.firebaseapp.com",
+  databaseURL: "https://storytelling-679f0-default-rtdb.firebaseio.com",
+  projectId: "storytelling-679f0",
+  storageBucket: "storytelling-679f0.appspot.com",
+  messagingSenderId: "1089312539694",
+  appId: "1:1089312539694:web:87b04a835680e482fe8bbf"
+};
+
+
